4c517c1c6445aa5b4c0649d50e1895ad46edc6b1
wangzengqing <925551747@qq.com>
2021-08-12 11:34:14 +0800

    新增消费者controller

--- 01-consumer-8080/src/main/java/com/abc/configure/DepartCodeConfigure.java
+++ 01-consumer-8080/src/main/java/com/abc/configure/DepartCodeConfigure.java
@@ -6,7 +6,7 @@ import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class DepartCodeConfigure {
-    @Bean  //RestTemplate spring 3.0开始提供的一种支持的一个 HTTP 请求工具
+    @Bean  //RestTemplate spring 3.0开始提供的一种支持的一个 HTTP  请求工具
     public RestTemplate restTemplate(){
        return new RestTemplate();
    }
