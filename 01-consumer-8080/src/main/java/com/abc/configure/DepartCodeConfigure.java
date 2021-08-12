86f7c1a96b6b68b3e14480d393e07739083256df
wangzengqing <925551747@qq.com>
2021-08-12 11:32:13 +0800

    新增消费者controller

---
+++ 01-consumer-8080/src/main/java/com/abc/configure/DepartCodeConfigure.java
@@ -0,0 +1,13 @@
+package com.abc.configure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class DepartCodeConfigure {
+    @Bean  //RestTemplate spring 3.0开始提供的一种支持的一个 HTTP 请求工具
+    public RestTemplate restTemplate(){
+       return new RestTemplate();
+   }
+}
--- 01-consumer-8080/src/main/java/com/abc/controller/SomeController.java
+++ 01-consumer-8080/src/main/java/com/abc/controller/SomeController.java
@@ -1,7 +1,44 @@
 package com.abc.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.abc.bean.Depart;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
-@RestController
+import java.util.List;
+
+@RestController //Controller, RestController的共同点都是用来表示Spring某个类的是否可以接收HTTP请求
+               // @Controller+@ResponseBody    返回页面JSON XML或自定义mediaType内容到页面
+               // @Controller
+@RequestMapping("/consumer/depart")
 public class SomeController {
+    @Autowired
+    private RestTemplate restTemplate;
+    String SERVICE_PROVIDER = "http://localhost:8081";
+    @PostMapping("/save")
+    public boolean saveHandler(@RequestBody Depart depart){
+
+        String url = SERVICE_PROVIDER + "/provider/depart/save";
+        return restTemplate.postForObject(url,depart,Boolean.class);
+    }
+    @DeleteMapping("/del/{id}")
+    public void deleteHandler(@PathVariable Integer id){
+        String url = SERVICE_PROVIDER + "/provider/depart/del";
+         restTemplate.delete(url);
+    }
+    @PutMapping("/update")
+    public void updateHandler(@RequestBody Depart depart){
+        String url = SERVICE_PROVIDER + "/provider/depart/update";
+        restTemplate.put(url,depart);
+    }
+    @GetMapping("/get/{id}")
+    public Depart getByIdHandler(@PathVariable Integer id){
+        String url = SERVICE_PROVIDER + "/provider/depart/get";
+        return restTemplate.getForObject(url,Depart.class);
+    }
+    @GetMapping("/list")
+    public List<Depart> list(){
+        String url = SERVICE_PROVIDER + "/provider/depart/list";
+        return restTemplate.getForObject(url,List.class);
+    }
 }
