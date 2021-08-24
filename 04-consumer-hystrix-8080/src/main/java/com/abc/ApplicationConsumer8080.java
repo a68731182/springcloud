8ec9f1363a9178b63050dbee81b929e8d457c165
wangzengqing <925551747@qq.com>
2021-08-24 14:30:31 +0800

    04-05:hystrix服务熔断与降级:dashboard仪表盘工程创建

--- 04-consumer-hystrix-8080/src/main/java/com/abc/ApplicationConsumer8080.java
+++ 04-consumer-hystrix-8080/src/main/java/com/abc/ApplicationConsumer8080.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 //@EnableCircuitBreaker   开启熔断器
 //@SpringBootApplication
+@EnableHystrixDashboard  // 开启Dashboard功能
 @EnableFeignClients  //开启Feign客户端
 @SpringCloudApplication
 public class ApplicationConsumer8080 {
